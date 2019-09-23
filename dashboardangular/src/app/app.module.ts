cf52b7b4c3c2eef7c677e603f6a1a539bc8dddea
D3NCE0NLINE <39002805+D3NCE0NLINE@users.noreply.github.com>
2019-09-23 15:38:36 +0200

    styles not working

--- dashboardangular/src/app/app.module.ts
+++ dashboardangular/src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidenavComponent,
   ],
   imports: [
     BrowserModule,
